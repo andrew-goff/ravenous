c7b7ee531b83cf98d9702de962c2ac81c7bf0be1
andrew-goff <andrew.goff@hotmail.co.uk>
2023-05-17 16:46:48 +0100

    Updated App and Business.js
    Created a new component called Business.js for he Ravenous app

---
+++ src/business.js
@@ -0,0 +1,19 @@
+function Business(){
+    const name = '';
+    const address = '';
+    const zipcode = '';
+    const category = 'Takeaway';
+    const rating = 4;
+    
+    return(
+        <div>
+            <p>{name}</p>
+            <p>{address}</p>
+            <p>{zipcode}</p>
+            <p>{category}</p>
+            <p>{rating}</p>
+        </div>
+    );
+}
+
+export default Business;
