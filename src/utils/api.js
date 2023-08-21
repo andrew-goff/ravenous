2e42b1cf38d3d3205e884b292916cd84e4793816
andrew-goff <andrew.goff@hotmail.co.uk>
2023-08-21 16:45:39 +0100

    Updated API response statement.

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,19 @@
 const apiKey = 'VpeVD2_ZS6wsKAM9DM9YV5o2gr7-gXkOKiKcWgUW-XOxBYWoY9l1Qh45UTds4jBYwwiWg9tRTlSjGRaxoSTr2aZUUgltan980MCqUomL4eFoVLp_lTCJWN-m0EvjZHYx';
+const apiBaseUrl = 'https://api.yelp.com/v3';
 
+const businessListings = async() => {
+    const businessEndpoint = '/business';
+    const requestParams = `?api_key=${apikey}`;
+    const urlToFetch = businessEndpoint + requestParams;
 
+    try {
+        const response = await fetch(urlToFetch);
+        
+        if (response.ok === true){
+
+        }
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
